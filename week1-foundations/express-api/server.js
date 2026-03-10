93d3bdc62a5a71afe74c867a54eb6cea322e8360
keyan-moodley <keyanmoodley21@gmail.com>
2026-03-10 16:25:21 +0200

    Day 6: PostgreSQL integration with Express API

--- week1-foundations/express-api/server.js
+++ week1-foundations/express-api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const pool = require("./db.js")
+
 const app = express();
 
 const PORT = 3000;
@@ -14,27 +16,36 @@ app.get("/", (req, res) => {
     res.send("My first Express server!");
 });
 
-app.get("/users", (req, res) => {
-    res.json(users);
+app.get("/users", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result.rows)
+    } catch (error) {
+        console.log("Error: " + error);
+        res.status(500).send("Server error");
+    }
 });
 
-app.post("/users", (req, res) => {
-    const newUser = {
-        id: users.length +1,
-        name: req.body.name
-    };
-
-    users.push(newUser);
-
-    res.json(newUser);
+app.post("/users", async (req, res) => {
+    try {
+        const name = req.body.name;
+        const addUser = await pool.query("INSERT INTO users (name) VALUES ($1) RETURNING *", [name]);
+        res.json(addUser.rows[0])
+    } catch (error) {
+        console.log("Error: " + error);
+        res.status(500).send("Server error")
+    }
 });
 
-app.delete("/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    users = users.filter(user => user.id !== id);
-
-    res.json({message: "User deleted"});
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        await pool.query("DELETE FROM users WHERE id = $1", [id]);
+        res.json({message: `User ${id} was deleted`})
+    } catch (error) {
+        console.log("Error: " + error);
+        res.status(500).send("Server error")
+    }
 });
 
 app.listen(PORT, () => {
